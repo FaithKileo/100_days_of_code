a12e6d3a49ef1f5f5b832cbe3710adf0ee3f7e17
FaithKileo <faithfrank344@gmail.com>
2022-04-20 20:26:55 +0300

    added sender_sms

---
+++ Restaurant_management/sendsms.py
@@ -0,0 +1,25 @@
+import requests
+
+# importing twilio - Make sure to install twilio with pip install twilio
+from twilio.rest import Client
+
+# initialize a twilio client
+client = Client("AC7580b24a06fffa8ca4d762c5c9053901", "86dd6314aa511821ebaf83227dcad537")
+
+def jack_send_sms(to, msg: str):
+    try:
+        for t in to.split(","):
+            message = client.messages \
+                        .create(
+                            body=msg,
+                            from_='+12182858596',
+                            to=t.strip()
+                        )
+            print(f"Sending sms to "+t)
+    except:
+        return "send failed"
+    
+
+err = jack_send_sms("+255712111936","Faith")
+print(err)
+# print("hgdfhjjkdf")
